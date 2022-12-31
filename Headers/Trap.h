3eb38418a376e5e4e8a88c694b4136744936bd66
kit0ra <noovaomega@gmail.com>
2022-12-31 20:02:22 +0100

    Add trap.h header file

---
+++ Headers/Trap.h
@@ -0,0 +1,19 @@
+#ifndef PROJET_PROGRAMMATION_S5_TRAP_H
+#define PROJET_PROGRAMMATION_S5_TRAP_H
+
+#include <utility>  // for std::pair
+#include <iostream>
+
+class Trap {
+public:
+    Trap(std::pair<int, int> position);
+    std::pair<int, int> getPosition() const;
+    void setPosition(std::pair<int, int> position);
+    void setSymbol(char symbol);
+    char getSymbol() const;
+private:
+    std::pair<int, int> d_position;
+    char d_symbol;
+};
+
+#endif //PROJET_PROGRAMMATION_S5_TRAP_H
