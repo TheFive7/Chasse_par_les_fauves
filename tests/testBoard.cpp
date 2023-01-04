dae5d0063890ed255a691f93495bb7f712a7f7f9
kit0ra <noovaomega@gmail.com>
2023-01-04 20:25:02 +0100

    Update grouped tests in testBoard.cpp and testPlayer.cpp in TEST_SUITE

--- tests/testBoard.cpp
+++ tests/testBoard.cpp
@@ -2,30 +2,37 @@
 
 #include "../Headers/Creation/Board.h"
 
-
-TEST_CASE("Test the constructor of the Board class") {
-    SUBCASE("Test the default constructor"){
-        Board board1;
-        CHECK(board1.getWidth() == 0);
-        CHECK(board1.getLength() == 0);
+TEST_SUITE("Test Board class")
+{
+    TEST_CASE("Test the constructor of the Board class")
+    {
+        SUBCASE("Test the default constructor")
+        {
+            Board board1;
+            CHECK(board1.getWidth() == 0);
+            CHECK(board1.getLength() == 0);
+        }
+        SUBCASE("Test the parameterized constructor")
+        {
+            Board board2(5, 7);
+            CHECK(board2.getWidth() == 5);
+            CHECK(board2.getLength() == 7);
+        }
     }
-    SUBCASE("Test the parameterized constructor") {
-        Board board2(5, 7);
-        CHECK(board2.getWidth() == 5);
-        CHECK(board2.getLength() == 7);
+
+    TEST_CASE("Test the getters and setters of the Board class")
+    {
+        Board board;
+        SUBCASE("Test the getters and setters for the width")
+        {
+            board.setWidth(10);
+            CHECK(board.getWidth() == 10);
+        }
+        SUBCASE("Test the getters and setters for the length")
+        {
+            board.setLength(15);
+            CHECK(board.getLength() == 15);
+        }
     }
 }
-
-TEST_CASE("Test the getters and setters of the Board class") {
-    Board board;
-    SUBCASE("Test the getters and setters for the width") {
-        board.setWidth(10);
-        CHECK(board.getWidth() == 10);
-    }
-    SUBCASE("Test the getters and setters for the length") {
-        board.setLength(15);
-        CHECK(board.getLength() == 15);
-    }
-}
-
 
